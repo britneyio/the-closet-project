3706b84549a66244d4703c8422e5aa6053c101aa
ise <okhiria.b@northeastern.edu>
2024-07-31 14:03:15 -0400

    deleting backend

--- backend/apps/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-# adds enpoints to our applicatoin
-accounts_urlpatterns = [
-    path(r'api/v1/', include('djoser.urls')),
-    path(r'api/v1/', include('djoser.urls.authtoken'))
-]
--- backend/apps/closet/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from apps.closet.models import ClothingItem, ClothingType, Outfit
-# Register your models here.
-
-admin.site.register(ClothingType)
-admin.site.register(ClothingItem)
-admin.site.register(Outfit)
