1176abd62673b346c3d3b864df15b3654505de34
ise <okhiria.b@northeastern.edu>
2024-07-31 17:38:26 -0400

    fixed database conditional

--- backend/settings.py
+++ backend/settings.py
@@ -145,13 +145,13 @@ if IS_HEROKU_APP:
             ssl_require=True,
         ),
     }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
